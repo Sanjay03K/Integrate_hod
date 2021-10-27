c0f43bd953784007a2bde8114409e6118a8c8f94
Sanjay03K <75990210+Sanjay03K@users.noreply.github.com>
2021-10-27 20:09:22 +0530

    Add files via upload

---
+++ src/variables/general.js
@@ -0,0 +1,1089 @@
+export const tablesTableData1 = [
+  {
+    sno: "1",
+    name: "Alison Roy X",
+    email: "alison.23cs@licet.ac.in",
+    reg: "311120103907",
+    roll: "19CS045",
+  },
+  {
+    sno: "2",
+    name: "Auskin Immanuel J",
+    email: "auskinimmanuel.24cs@licet.ac.in",
+    reg: "311120104012",
+    roll: "20CS046",
+  },
+  {
+    sno: "3",
+    name: "Gautham S",
+    email: "gautham.24cs@licet.ac.in",
+    reg: "311120104017",
+    roll: "20CS008",
+  },
+  {
+    sno: "4",
+    name: "Ashwin",
+    email: "ashwin.23cs@licet.ac.in",
+    reg: "311120103904",
+    roll: "19CS051",
+  },
+];
+export const tablesTableData2 = [
+  {
+    sno: "1",
+    name: "Ashwin",
+    email: "ashwin.23cs@licet.ac.in",
+    reg: "311120103904",
+    roll: "19CS051",
+  },
+  {
+    sno: "2",
+    name: "Auskin Immanuel J",
+    email: "auskinimmanuel.24cs@licet.ac.in",
+    reg: "311120104012",
+    roll: "20CS046",
+  },
+  {
+    sno: "3",
+    name: "Gautham S",
+    email: "gautham.24cs@licet.ac.in",
+    reg: "311120104017",
+    roll: "20CS008",
+  },
+
+  {
+    sno: "4",
+    name: "Alison Roy X",
+    email: "alison.23cs@licet.ac.in",
+    reg: "311120103907",
+    roll: "19CS045",
+  },
+];
+export const tablesTableData3 = [
+  {
+    sno: "1",
+    name: "Auskin Immanuel J",
+    email: "auskinimmanuel.24cs@licet.ac.in",
+    reg: "311120104012",
+    roll: "20CS046",
+  },
+  {
+    sno: "2",
+    name: "Gautham S",
+    email: "gautham.24cs@licet.ac.in",
+    reg: "311120104017",
+    roll: "20CS008",
+  },
+
+  {
+    sno: "3",
+    name: "Alison Roy X",
+    email: "alison.23cs@licet.ac.in",
+    reg: "311120103907",
+    roll: "19CS045",
+  },
+  {
+    sno: "4",
+    name: "Ashwin",
+    email: "ashwin.23cs@licet.ac.in",
+    reg: "311120103904",
+    roll: "19CS051",
+  },
+];
+export const tablesTableData4 = [
+  {
+    sno: "2",
+    name: "Gautham S",
+    email: "gautham.24cs@licet.ac.in",
+    reg: "311120104017",
+    roll: "20CS008",
+  },
+  {
+    sno: "1",
+    name: "Auskin Immanuel J",
+    email: "auskinimmanuel.24cs@licet.ac.in",
+    reg: "311120104012",
+    roll: "20CS046",
+  },
+
+  {
+    sno: "3",
+    name: "Ashwin",
+    email: "ashwin.23cs@licet.ac.in",
+    reg: "311120103904",
+    roll: "19CS051",
+  },
+  {
+    sno: "4",
+    name: "Alison Roy X",
+    email: "alison.23cs@licet.ac.in",
+    reg: "311120103907",
+    roll: "19CS045",
+  },
+];
+export const GeneralParticulars = [
+  {
+    field: "Student Name",
+    data: "NULL",
+  },
+  {
+    field: "ID",
+    data: "NULL",
+  },
+  {
+    field: "row5 Register No.",
+    data: "NULL",
+  },
+  {
+    field: "Sex",
+    data: "NULL",
+  },
+  {
+    field: "Date Of Birth",
+    data: "NULL",
+  },
+  {
+    field: "Nationality",
+    data: "NULL",
+  },
+  {
+    field: "Religion",
+    data: "NULL",
+  },
+  {
+    field: "If Catholic, Parish?",
+    data: "NULL",
+  },
+  {
+    field: "Dalit Catholic",
+    data: "NULL",
+  },
+  {
+    field: "Comunity",
+    data: "NULL",
+  },
+  {
+    field: "Blood Group",
+    data: "NULL",
+  },
+  {
+    field: "Mother Tounge",
+    data: "NULL",
+  },
+  {
+    field: "Contact Number",
+    data: "NULL",
+  },
+  {
+    field: "Aadhar No.",
+    data: "NULL",
+  },
+  {
+    field: "Official Email ID",
+    data: "NULL",
+  },
+  {
+    field: "Personal Email ID",
+    data: "NULL",
+  },
+  {
+    field: "Present Address",
+    data: "NULL",
+  },
+  {
+    field: "Permanent Address",
+    data: "NULL",
+  },
+];
+export const HigherSecondaryDetails = [
+  {
+    field: "Name of the School",
+    data: "NULL",
+  },
+  {
+    field: "Board",
+    data: "NULL",
+  },
+  {
+    field: "Medium Of Instruction",
+    data: "NULL",
+  },
+  {
+    field: "Second Language",
+    data: "NULL",
+  },
+  {
+    field: "Group",
+    data: "NULL",
+  },
+  {
+    field: "Total Marks",
+    data: "NULL",
+  },
+  {
+    field: "Overall Percentage",
+    data: "NULL",
+  },
+  {
+    field: "Cut-Off Marks",
+    data: "NULL",
+  },
+  {
+    field: "No. Of Attempts",
+    data: "NULL",
+  },
+];
+export const SecondaryDetails = [
+  {
+    field: "Name of the School",
+    data: "NULL",
+  },
+  {
+    field: "Board",
+    data: "NULL",
+  },
+  {
+    field: "Medium Of Instruction",
+    data: "NULL",
+  },
+  {
+    field: "Second Language",
+    data: "NULL",
+  },
+  {
+    field: "Group",
+    data: "NULL",
+  },
+  {
+    field: "Total Marks",
+    data: "NULL",
+  },
+  {
+    field: "Overall Percentage",
+    data: "NULL",
+  },
+  {
+    field: "Cut-Off Marks",
+    data: "NULL",
+  },
+  {
+    field: "No. Of Attempts",
+    data: "NULL",
+  },
+];
+export const Father = [
+  {
+    field: "Father's Name",
+    data: "NULL",
+  },
+  {
+    field: "Father's Qualification",
+    data: "NULL",
+  },
+  {
+    field: "Fathers Occupation",
+    data: "NULL",
+  },
+  {
+    field: "Father's Office Address",
+    data: "NULL",
+  },
+  {
+    field: "Father's Annual Income",
+    data: "NULL",
+  },
+  {
+    field: "Father's Mobile No.",
+    data: "NULL",
+  },
+  {
+    field: "Father's Email ID",
+    data: "NULL",
+  },
+  {
+    field: "Guardian Name",
+    data: "NULL",
+  },
+  {
+    field: "Guardian Qualification",
+    data: "NULL",
+  },
+  {
+    field: "Guardian Occupation",
+    data: "NULL",
+  },
+  {
+    field: "Guardian Office Address",
+    data: "NULL",
+  },
+  {
+    field: "Guardian Annual Income",
+    data: "NULL",
+  },
+  {
+    field: "Guardian Mobile No.",
+    data: "NULL",
+  },
+  {
+    field: "Guardian Email ID",
+    data: "NULL",
+  },
+];
+export const Mother = [
+  {
+    field: "Mother's Name",
+    data: "NULL",
+  },
+  {
+    field: "Mother's Qualification",
+    data: "NULL",
+  },
+  {
+    field: "Mother's Occupation",
+    data: "NULL",
+  },
+  {
+    field: "Mother's Office Address",
+    data: "NULL",
+  },
+  {
+    field: "Mother's Annual Income",
+    data: "NULL",
+  },
+  {
+    field: "Mother's Mobile No.",
+    data: "NULL",
+  },
+  {
+    field: "Mother's Email ID",
+    data: "NULL",
+  },
+  {
+    field: "No. Of Siblings",
+    data: "NULL",
+  },
+  {
+    field: "Qualification Of Siblings",
+    data: "NULL",
+  },
+  {
+    field: "Occupation",
+    data: "NULL",
+  },
+  {
+    field: "Alumni Of LICET Or LOYOLA",
+    data: "NULL",
+  },
+];
+export const AdmissionDetails = [
+  {
+    field: "Application No.",
+    data: "NULL",
+  },
+  {
+    field: "Date Of Admission",
+    data: "NULL",
+  },
+  {
+    field: "Quota",
+    data: "NULL",
+  },
+  {
+    field: "Regular / Lateral Entry",
+    data: "NULL",
+  },
+  {
+    field: "Day Scholar / Hosteller",
+    data: "NULL",
+  },
+];
+export const ScholarshipDetails = [
+  {
+    field: "Scholarship Name",
+    data: "NULL",
+  },
+  {
+    field: "Scholarship Amount",
+    data: "NULL",
+  },
+  {
+    field: "First Graduate",
+    data: "NULL",
+  },
+  {
+    field: "Economically Backward (Annual Income Of Parents Less than 1LPA)",
+    data: "NULL",
+  },
+];
+export const TechnicalSkills = [
+  {
+    field: "Programming Languages",
+    data: "NULL",
+  },
+  {
+    field: "Operating Systems",
+    data: "NULL",
+  },
+  {
+    field: "DataBase",
+    data: "NULL",
+  },
+  {
+    field: "Software Profficieny",
+    data: "NULL",
+  },
+  {
+    field: "Other Skills",
+    data: "NULL",
+  },
+];
+export const NonTechnicalSkills = [
+  {
+    field: "Aptitude / Analytical Skills",
+    data: "NULL",
+  },
+  {
+    field: "Communication Skills",
+    data: "NULL",
+  },
+  {
+    field: "Social Media Exposure",
+    data: "NULL",
+  },
+  {
+    field: "Leadership Skills",
+    data: "NULL",
+  },
+  {
+    field: "Other Interpersonal Skills",
+    data: "NULL",
+  },
+];
+export const Goals = [
+  {
+    field: "Short Term Goals",
+    data: "NULL",
+  },
+  {
+    field: "Long Term Goals",
+    data: "NULL",
+  },
+];
+export const ProfessionalMembership = [
+  {
+    field: "Name",
+    data: "NULL",
+  },
+  {
+    field: "Membership No.",
+    data: "NULL",
+  },
+  {
+    field: "Duration",
+    data: "NULL",
+  },
+];
+export const Academicinfo = [
+  {
+    row1: "Subject 1",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Subject 2",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Subject 3",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Subject 4",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Subject 5",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Subject 6",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Lab 1",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Lab 2",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Lab 3",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Attendance Percentage",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Credits Earned",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "Total No. Of Arrears",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "GPA",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+];
+export const AcademicSummary = [
+  {
+    row1: "Overall attendance percentage",
+    row2: "NULL",
+  },
+  {
+    row1: "CGPA",
+    row2: "NULL",
+  },
+  {
+    row1: "Total Credits Earned",
+    row2: "NULL",
+  },
+  {
+    row1: "Eligible for placements",
+    row2: "NULL",
+  },
+];
+export const International = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+    row6: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+    row6: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+    row6: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+    row6: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+    row6: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+    row6: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+    row6: "NULL",
+  },
+];
+export const IndustrialVist = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+];
+export const InplantTraining = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+];
+export const GuestLecture = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const MotivationalTalks = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const Internships = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const ValueAddedCourse = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const Workshop = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const Webinar = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const Competition = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const PlacementTraining = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const MiniProject = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const FinalProject = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const Publications = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+    row5: "NULL",
+  },
+];
+export const Clubs = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const OutReachActivity = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+];
+export const SportsAchievements = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+    row4: "NULL",
+  },
+];
+export const Culturals = [
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+  {
+    row1: "NULL",
+    row2: "NULL",
+    row3: "NULL",
+  },
+];
+export const Feedback = [
+  // {
+  //   row1: "NULL",
+  // },
+];
