184d6704e42c685c7ee8a56352cd74c1d2a93688
Auskin26 <75899129+Auskin26@users.noreply.github.com>
2021-10-28 01:16:25 +0530

    search layout

--- src/views/Dashboard/GeneralInformation.js
+++ src/views/Dashboard/GeneralInformation.js
@@ -11,17 +11,16 @@ import {
   Thead,
   Tr,
   useColorModeValue,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  Select,
-  SimpleGrid,
+  IconButton,
   Button,
-  ButtonGroup,
+  Input,
+  InputGroup,
+  InputLeftElement,
   useDisclosure,
-  Collapse,
+  SimpleGrid,
+  Box,
 } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 // Custom components
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
@@ -32,16 +31,20 @@ import { SearchBar } from "components/Navbars/SearchBar/SearchBar";
 
 function GeneralInformation() {
   const [data, setData] = useState([]);
-  let params = new URLSearchParams;
+  let params = new URLSearchParams();
+  const [searchTerm, setSearchTerm] = useState("");
 
-  params.append("department",localStorage.getItem("dept"))
+  params.append("department", localStorage.getItem("dept"));
   useEffect(async () => {
-    axios.post("http://localhost:5000/GeneralHOD",params).then((items) => {
+    axios.post("http://localhost:5000/GeneralHOD", params).then((items) => {
       setData(items.data);
     });
   });
-  const { isOpen, onToggle } = useDisclosure();
+
   const textColor = useColorModeValue("gray.700", "white");
+  const inputBg = useColorModeValue("white", "gray.800");
+  const mainorange = useColorModeValue("orange.300", "orange.300");
+  const searchIconColor = useColorModeValue("gray.700", "gray.200");
 
   return (
     <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
@@ -51,73 +54,176 @@ function GeneralInformation() {
             <Text fontSize="xl" color={textColor} fontWeight="bold" mr="auto">
               General Information
             </Text>
-            <Selection />
-            <Flex direction="column" align="flex-end" w="100%">
-              <Button
-                onClick={onToggle}
-                marginTop="3rem"
-                marginBottom="1rem"
-                marginRight="1rem"
-                colorScheme="orange"
-                variant="solid"
-              >
-                View
-              </Button>
-            </Flex>
           </Flex>
         </CardBody>
-        <Collapse in={isOpen} animateOpacity>
-          <Card>
-            <CardHeader>Search Student</CardHeader>
-            <SearchBar />
-            <br />
-            <Button
-              colorScheme="orange"
-              alignSelf="flex-end"
-              variant="solid"
-              width="25%"
+        <SimpleGrid columns={{ sm: 1, md: 2, xl: 2 }} gap={5}>
+          <Box>
+            <CardHeader mt="1em">
+              <Text fontSize="lg" color={textColor} fontWeight="semi">
+                Search Batch
+              </Text>
+            </CardHeader>
+
+            <InputGroup
+              bg={inputBg}
+              mt="1rem"
+              borderRadius="15px"
+              w="cover"
+              _focus={{
+                borderColor: { mainorange },
+              }}
+              _active={{
+                borderColor: { mainorange },
+              }}
             >
-              Download
-            </Button>
-          </Card>
-        </Collapse>
+              <InputLeftElement
+                children={
+                  <IconButton
+                    bg="inherit"
+                    borderRadius="inherit"
+                    _hover="none"
+                    _active={{
+                      bg: "inherit",
+                      transform: "none",
+                      borderColor: "transparent",
+                    }}
+                    _focus={{
+                      boxShadow: "none",
+                    }}
+                    icon={
+                      <SearchIcon color={searchIconColor} w="15px" h="15px" />
+                    }
+                  ></IconButton>
+                }
+              />
+
+              <Input
+                onChange={(event) => setSearchTerm(event.target.value)}
+                fontSize="xs"
+                py="11px"
+                placeholder="Type here..."
+                borderRadius="inherit"
+                value={searchTerm}
+              />
+            </InputGroup>
+          </Box>
+
+          <Box>
+            <CardHeader mt="1em">
+              <Text fontSize="lg" color={textColor} fontWeight="semi">
+                Search Student
+              </Text>
+            </CardHeader>
+            <InputGroup
+              bg={inputBg}
+              mt="1rem"
+              borderRadius="15px"
+              w="cover"
+              _focus={{
+                borderColor: { mainorange },
+              }}
+              _active={{
+                borderColor: { mainorange },
+              }}
+            >
+              <InputLeftElement
+                children={
+                  <IconButton
+                    bg="inherit"
+                    borderRadius="inherit"
+                    _hover="none"
+                    _active={{
+                      bg: "inherit",
+                      transform: "none",
+                      borderColor: "transparent",
+                    }}
+                    _focus={{
+                      boxShadow: "none",
+                    }}
+                    icon={
+                      <SearchIcon color={searchIconColor} w="15px" h="15px" />
+                    }
+                  ></IconButton>
+                }
+              />
+
+              <Input
+                onChange={(event) => setSearchTerm(event.target.value)}
+                fontSize="xs"
+                py="11px"
+                placeholder="Type here..."
+                borderRadius="inherit"
+                value={searchTerm}
+              />
+            </InputGroup>
+          </Box>
+        </SimpleGrid>
+        <Button
+          mt="1em"
+          onClick="m"
+          colorScheme="orange"
+          alignSelf="flex-end"
+          variant="solid"
+          width="25%"
+        >
+          Download Report
+        </Button>
       </Card>
-      <Collapse in={isOpen} animateOpacity>
-        <Card overflowX={{ sm: "scroll", xl: "hidden" }}>
-          <CardHeader p="6px 0px 22px 0px">
-            <Text fontSize="xl" color={textColor} fontWeight="bold">
-              Students List
-            </Text>
-          </CardHeader>
-          <CardBody>
-            <Table variant="simple" color={textColor}>
-              <Thead>
-                <Tr my=".8rem" pl="0px" color="gray.400">
-                  <Th color="gray.400">Roll No.</Th>
-                  <Th color="gray.400">Name</Th>
-                  <Th color="gray.400">Register No</Th>
-                  <Th color="gray.400">Batch</Th>
-                  <Th color="gray.400">Email</Th>
-                  <Th></Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {data.map((item) => {
+      <Card overflowX={{ sm: "scroll", xl: "hidden" }}>
+        <CardHeader p="6px 0px 22px 0px">
+          <Text fontSize="xl" color={textColor} fontWeight="bold">
+            Students List
+          </Text>
+        </CardHeader>
+        <CardBody>
+          <Table variant="simple" color={textColor}>
+            <Thead>
+              <Tr my=".8rem" pl="0px" color="gray.400">
+                <Th color="gray.400">Roll No.</Th>
+                <Th color="gray.400">Name</Th>
+                <Th color="gray.400">Register No</Th>
+                <Th color="gray.400">Batch</Th>
+                <Th color="gray.400">Email</Th>
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data
+                .filter((item) => {
+                  if (searchTerm == "") {
+                    return item;
+                  } else if (
+                    item.sname
+                      .toLowerCase()
+                      .includes(searchTerm.toLocaleLowerCase()) ||
+                    item.roll_no
+                      .toLowerCase()
+                      .includes(searchTerm.toLocaleLowerCase()) ||
+                    item.batch
+                      .toLowerCase()
+                      .includes(searchTerm.toLocaleLowerCase()) ||
+                    item.reg_no
+                      .toLowerCase()
+                      .includes(searchTerm.toLocaleLowerCase())
+                  ) {
+                    return item;
+                  }
+                })
+                .map((item) => {
                   return (
                     <GeneralParticularstablerow
-                    roll={item.roll_no}
-                    name={item.sname}
-                    reg={item.reg_no}
-                    batch={item.batch}
-                    email={item.licet_email}
-                  />
+                      roll={item.roll_no}
+                      name={item.sname}
+                      reg={item.reg_no}
+                      batch={item.batch}
+                      email={item.licet_email}
+                    />
                   );
                 })}
-              </Tbody>
-            </Table>
-          </CardBody>
-        </Card>
-      </Collapse>
+            </Tbody>
+          </Table>
+        </CardBody>
+      </Card>
     </Flex>
   );
 }
