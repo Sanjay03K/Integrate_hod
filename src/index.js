701eaac38f036adaf190fb02df5acff2f07218ab
sinking8 <ashwinblaze111@gmail.com>
2021-10-28 17:38:45 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import AuthLayout from "layouts/Auth.js";
-
 import AdminLayout from "layouts/Admin.js";
 import AdminLayout2 from "layouts/Admin2.js";
 
@@ -15,7 +14,6 @@ ReactDOM.render(
       <Route path={`/auth`} component={AuthLayout} />
       <Route path={`/admin`} component={AdminLayout} />
       <Route path={`/admin2`} component={AdminLayout2} />
-
       <Redirect from={`/`} to="/auth/SignIn" />
     </Switch>
   </HashRouter>,
