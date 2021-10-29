db8f5acaaed98aa010d6124cd1e3e9b950bfb6ff
Sanjay03K <75990210+Sanjay03K@users.noreply.github.com>
2021-10-29 12:44:48 +0530

    Add files via upload

--- src/controller/changepassword.js
+++ src/controller/changepassword.js
@@ -8,19 +8,19 @@ export default function change_pass() {
   const pcbtn = document.getElementById("pass-button");
   pcbtn.disabled = true;
   pcbtn.innerHTML = `<div class="spinner-border text-primary p-2 spinner-border-sm" role="status" aria-hidden="true"><span class="visually-hidden">Loading...</span></div>`;
-
   var oldPass = document.getElementById("old-pass").value;
   var newPass = document.getElementById("new-pass").value;
   var rePass = document.getElementById("re-pass").value;
-
-  if (oldPass === rePass) {
+  console.log(oldPass);
+  if (newPass == rePass) {
     let params = new URLSearchParams();
-    params.append("new-pass", newPass);
-    params.append("old-pass", oldPass);
-    params.append("email", localStorage.email);
+    params.append("newpass", newPass);
+    params.append("oldpass", oldPass);
+    params.append("email", localStorage.useremail);
     params.append("auth_token", localStorage.auth_token);
-
+    console.log(localStorage.auth_token);
     axios.post("http://localhost:5000/passchange", params).then((result) => {
+      console.log(result.data);
       if (result.data === "pass-fail") {
         document.getElementById("pass-fail").style.display = "block";
         document.getElementById("pass-mis").style.display = "none";
