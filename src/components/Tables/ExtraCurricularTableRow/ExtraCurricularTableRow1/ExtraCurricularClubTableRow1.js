e03064277ec1ff85c0b07625e40c0725635e4c7e
Sanjay03K <75990210+Sanjay03K@users.noreply.github.com>
2021-10-31 21:01:46 +0530

    Add files via upload

--- src/components/Tables/ExtraCurricularTableRow/ExtraCurricularTableRow1/ExtraCurricularClubTableRow1.js
+++ src/components/Tables/ExtraCurricularTableRow/ExtraCurricularTableRow1/ExtraCurricularClubTableRow1.js
@@ -1,11 +1,58 @@
 //Class Advisor ExtraCurricular Club TableRow
-
-import { Flex, Td, Text, Tr, useColorModeValue } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Input,
+  Td,
+  Text,
+  Tr,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+import axios from "axios";
 import React from "react";
 
+var server_URL = "http://localhost:5000/";
+
 function ExtraCurricularTableRow(props) {
-  const { row1, row2, row3, row4, row5, row6 } = props;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const { id,row1, row2, row3, row4, row5, row6 } = props;
   const textColor = useColorModeValue("gray.700", "white");
+
+  function funedit() {
+    let cid = { id };
+    let params = new URLSearchParams();
+    params.append("columnid", cid.id);
+    params.append("clubname", document.getElementById("ECID").value);
+    params.append("activity", document.getElementById("EDID").value);
+    params.append("date", document.getElementById("EPID").value);
+    params.append("outcome", document.getElementById("EOID").value);
+    params.append("credits", document.getElementById("EPDID").value);
+    axios.post(server_URL + "ExtraClubEdit", params);
+  }
+
+  function fundelete() {
+    let cid = { id };
+    let params = new URLSearchParams();
+    params.append("columnid", cid.id);
+    axios.post(server_URL + "ExtraClubDelete", params);
+  }
+
+  function funverify() {
+    let cid = { id };
+    let params = new URLSearchParams();
+    params.append("columnid", cid.id);
+    params.append("verify", "Verified");
+    axios.post(server_URL + "ExtraClubVerify", params);
+  }
 
   return (
     <Tr>
@@ -42,6 +89,135 @@ function ExtraCurricularTableRow(props) {
         <Flex align="center" py=".8rem" minWidth="100%" flexWrap="nowrap">
           <Flex direction="column">{row6}</Flex>
         </Flex>
+      </Td>
+      <Td>
+        <Button
+          onClick={onOpen}
+          bg="orange.300"
+          alignSelf="flex-end"
+          width="fit-content"
+        >
+          Edit
+        </Button>
+        <Modal size="xl" isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Edit Details</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Tr>
+                <Td>
+                  <Text m="1em">Name of the club</Text>
+                </Td>
+                <Td>
+                  <Input
+                    minWidth="20em"
+                    borderRadius="5px"
+                    fontSize="sm"
+                    type="text"
+                    defaultValue={row1}
+                    id="ECID"
+                  />
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>
+                  <Text m="1em">Activity</Text>
+                </Td>
+                <Td>
+                  <Input
+                    minWidth="20em"
+                    borderRadius="5px"
+                    fontSize="sm"
+                    type="text"
+                    defaultValue={row2}
+                    id="EDID"
+                  />
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>
+                  <Text m="1em">Date & Year</Text>
+                </Td>
+
+                <Td>
+                  <Input
+                    minWidth="20em"
+                    borderRadius="5px"
+                    fontSize="sm"
+                    type="text"
+                    defaultValue={row3}
+                    id="EPID"
+                  />
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>
+                  <Text m="1em">Outcome</Text>
+                </Td>
+                <Td>
+                  <Input
+                    minWidth="20em"
+                    borderRadius="5px"
+                    fontSize="sm"
+                    type="text"
+                    defaultValue={row4}
+                    id="EOID"
+                  />
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>
+                  <Text m="1em">Credits</Text>
+                </Td>
+                <Td>
+                  <Input
+                    minWidth="20em"
+                    borderRadius="5px"
+                    fontSize="sm"
+                    type="text"
+                    defaultValue={row5}
+                    id="EPDID"
+                  />
+                </Td>
+              </Tr>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button
+                colorScheme="blue"
+                mr={3}
+                onClick={() => {
+                  funedit();
+                  onClose();
+                }}
+              >
+                Submit
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Td>
+      <Td>
+        <Button
+          onClick = {fundelete}
+          bg="orange.300"
+          alignSelf="flex-end"
+          width="fit-content"
+        >
+          Delete
+        </Button>
+      </Td>
+      <Td>
+        <Button
+          onClick={funverify}
+          bg="orange.300"
+          alignSelf="flex-end"
+          width="fit-content"
+          disabled={{ row6}.row6 == "Verified" ? true : false}
+        >
+          Verify
+        </Button>
       </Td>
     </Tr>
   );
